5d7b1a4ba835db4f76355014854ef4b16b0e4a69
yuuta20012014 <yuuta20012014@gmail.com>
2026-02-14 13:18:14 +0900

    feature/yutaブランチで初めてのコミット

---
+++ app/fukashiro/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function Home() {
+  return (
+        <div className="bg-blue-500 w-24 h-24"></div>
+  );
+}
