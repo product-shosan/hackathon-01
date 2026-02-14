e79c3ee68b4884c110ce24c90a4a560c87f625bb
wansonam11 <sosen7874@naver.com>
2026-02-14 11:47:46 +0900

    tes04

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={50}
-          height={20}
-          priority
-        />
+
         <div className="flex flex-col items-center gap-6 text-center">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             トイレ状況共有
@@ -34,7 +27,7 @@ export default function Home() {
             center.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row" style={{margin: '0 auto'}}>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row" style={{ margin: '0 auto' }}>
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-green-500 px-5 text-white transition-colors hover:bg-green-600 md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
