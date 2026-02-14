df1793b0bc91106e624281364c77890beb0e4dfc
yuuta20012014 <yuuta20012014@gmail.com>
2026-02-14 14:29:58 +0900

    たぶんできた(？)

--- app/fukashiro/page.tsx
+++ app/fukashiro/page.tsx
@@ -2,8 +2,48 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-export default function Home() {
-  return (
-        <div className="bg-blue-500 w-24 h-24"></div>
-  );
-}
+export default function AxiosGet() {
+    const [isInToilet, setIsInToilet] = useState<boolean | null>(null);
+
+    // の状態を定期的に取得するための関数
+    const fechtIsInToilet = async () => {
+        try {
+            const responset = await fetch('hogehoge.com/api/isInToilet');
+            const data = await responset.json();
+            // setIsInToilet(data.isInToilet);
+            setIsInToilet(data.isInToilet);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setTimeout(() => {console.log("Failed to fetch data after 5 seconds");}, 5000);
+        };
+    };
+    
+    useEffect(() => {
+        fechtIsInToilet(); // コンポーネントがマウントされたときに一度APIを呼び出す
+        const intervalId = setInterval(() => {
+            fechtIsInToilet();
+        }, 5000); // 5秒ごとにAPIを呼び出す
+
+        return () => clearInterval(intervalId); // コンポーネントがアンマウントされたときにインターバルをクリアする
+    }, []);
+    
+    const intervalId = setInterval(() => {
+        fechtIsInToilet();
+        return () => clearInterval(intervalId);
+    }, 5000) // 5秒ごとにAPIを呼び出す
+
+    return (
+        <>
+            <div>
+                {isInToilet === null ? (
+                    <p>Loading...</p>
+                ) : isInToilet ? (
+                    <p>トイレに入っています。</p>
+                ) : (
+                    <p>トイレに入っていません。</p>
+                )}
+            </div>
+            <p> デバッグ: {isInToilet}, </p>
+        </>
+    );
+};
