d477b6df59e75e6327dbba0947d120f6f639a1a4
wansonam11 <sosen7874@naver.com>
2026-02-14 12:04:48 +0900

    test04

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,12 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black">
 
         <div className="flex flex-col items-center gap-6 text-center">
+          <div className="text-6xl">🚹</div>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             トイレ状況共有
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+            SHOSANが提供する清潔サービス
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row" style={{ margin: '0 auto' }}>
